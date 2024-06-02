afff9aff231b536b59d0d1cbce53579fe972eacb
Ikutsu <zhuoxuan233@gmail.com>
2024-06-02 13:57:25 +0200

    AndroidMain: Add edge-to-edge support

--- composeApp/src/androidMain/kotlin/io/ikutsu/osumusic/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/ikutsu/osumusic/MainActivity.kt
@@ -1,24 +1,23 @@
 package io.ikutsu.osumusic
 
 import App
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
 
         setContent {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
