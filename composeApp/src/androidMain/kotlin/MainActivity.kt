4c338609baa992e2d946e2320bb6d93983028661
Ikutsu <zhuoxuan233@gmail.com>
2024-08-11 02:14:29 +0800

    androidMain: Add Splash screen

--- composeApp/src/androidMain/kotlin/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
