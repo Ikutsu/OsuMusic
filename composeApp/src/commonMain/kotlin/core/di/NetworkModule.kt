637a4e8b4533295e09ff41d37debb1ee33c79042
Ikutsu <zhuoxuan233@gmail.com>
2024-11-05 14:43:18 +0100

    commonMain/core: Ignore unknown keys in JSON serialization

--- composeApp/src/commonMain/kotlin/core/di/NetworkModule.kt
+++ composeApp/src/commonMain/kotlin/core/di/NetworkModule.kt
@@ -8,6 +8,7 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -17,7 +18,11 @@ val networkModule = module {
 
 val httpClient = HttpClient {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                ignoreUnknownKeys = true
+            }
+        )
     }
     install(HttpCache)
     install(Logging) {
